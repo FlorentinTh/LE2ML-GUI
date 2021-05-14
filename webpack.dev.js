7cc69cac0f849d2eea96bd2c97dd91c54ca002da
FlorentinTh <florentin.thullier1@uqac.ca>
2021-05-14 13:16:38 -0400

    build(webpack): improve speed for dev configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   devServer: {
     historyApiFallback: true,
     open: true,
-    compress: true,
+    compress: false,
     hot: true,
     port: 8080,
     quiet: true
